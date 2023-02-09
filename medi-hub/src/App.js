03dd7cd01c7203bfea239dd036d5337c2bf245b7
karkuvelpandi <karkuvelpandi007@gmail.com>
2023-02-09 20:02:58 +0530

    ok

---
+++ medi-hub/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import SocialMediaBar from './Common Components/Navbar/SocialMediaBar'
+
+const App = () => {
+  return <>
+  
+  <SocialMediaBar/>
+  
+  
+  
+  
+  
+  </>
+}
+
+export default App
---
+++ medi-hub/src/Common Components/Navbar/SocialMediaBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SocialMediaBar = () => {
+  return<>
+     <div className="container-lg bar zoomEffect">
+     hello
+     </div>
+  </>
+}
+
+export default SocialMediaBar
